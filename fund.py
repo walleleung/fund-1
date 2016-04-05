e6f02490c59a8d90cca1b36d2c20c7cc621400ff
JunlongPeng <pengjunlong@pengjunlong.local>
2016-04-05 18:25:05 +0800

    数据初始化

--- fund.py
+++ fund.py
@@ -1,4 +1,4 @@
-# coding=utf8
+#coding=utf-8
 import sys
 import httplib
 import json
@@ -12,7 +12,7 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def getFundData(fund_code, days):
+def getDailyData(fund_code, days):
     curtime = str(time.time()) + '353';
     httpClient = None
 
@@ -21,67 +21,20 @@ def getFundData(fund_code, days):
         httpClient.request('GET', '/trade/fund/jzlist?fund_code=' + fund_code + '&cur_page=1&page_size=' + str(days) + '&_=' + curtime)
 
         response = httpClient.getresponse()
-#    print response.status
-#    print response.reason
+        #    print response.status
+        #    print response.reason
         jijin_json = json.loads(response.read())
         data = jijin_json['data']['list']
-        cur_money = 10000.0
-        last_ljjz = 0.0
-        buy_days = 0
-        up_days = 0
-        down_days = 0
-        profit_times = 0
-        loss_times = 0
-
         data = sorted(data, key = lambda x:x['date'])
-        show_date = data[0]['date']
+        return data
+    except Exception, e:
+        print e
+    finally:
+        if httpClient:
+            httpClient.close()
 
-        for day_data in data:
-            cur_date = day_data['date']
-            cur_dwjz = day_data['dwjz']
-            cur_ljjz = day_data['ljjz']
-            cur_rzzl = 1+float(day_data['rzzl'])/100
-            if last_ljjz == 0.0:
-                last_ljjz = cur_ljjz
-            else:
-                if cur_rzzl > 1:
-                    up_days += 1
-                    down_days = 0
-                else:
-                    up_days = 0
-                    down_days += 1
-
-                if buy_days > 0:
-                    buy_days += 1
-
-                if down_days >= 2 and buy_days == 0:
-                    buy_days = 1
-
-                if buy_days == 1:
-                    cur_money *= 0.9988
-                    buy_money = cur_money
-                    #print cur_date + ":buy " + str(round(cur_money,2))
-                elif buy_days >= 3 and up_days > 0:
-                    cur_money *= cur_rzzl
-                    cur_money *= 0.995
-                    sell_money = cur_money
-                    if sell_money > buy_money:
-                        profit_times += 1
-                    else:
-                        loss_times += 1
-                    #print cur_date + ":sell " + str(round(cur_money,2)) + ", " + str(round(100*(cur_rzzl-1),2)) + "%"
-                    buy_days = 0
-                    down_days = 0
-                    up_days = 0
-                elif buy_days >= 2:
-                    cur_money *= cur_rzzl
-                    #print cur_date + ":hold " + str(round(100*(cur_rzzl-1),2)) + "%"
-                else:
-                    tmp=0
-                    #print "pass:" + cur_date
-
-                last_ljjz = cur_ljjz
-
+def getName(fund_code):
+    try:
         httpClient = httplib.HTTPConnection('so.hexun.com', 80, timeout=10)
         httpClient.request('GET', '/ajax.do?type=fund&key=' + fund_code)
         response = httpClient.getresponse()
@@ -89,37 +42,77 @@ def getFundData(fund_code, days):
         index_begin=fund_str.index('[')
         index_end=fund_str.index(']')
         json_data=json.loads(fund_str[index_begin+1:index_end])
-        ret = {
-                'date' : show_date,
-                'name' : json_data['name'],
-                'code' : json_data['code'],
-                'money' : cur_money,
-                'profit' : profit_times,
-                'loss' : loss_times
-                }
-        return ret
+        return json_data['name']
     except Exception, e:
         print e
     finally:
         if httpClient:
-            httpClient.close()
+            httpClient.close
 
 
-#fund_code = raw_input("请输入基金代号：")
-days = sys.argv[1] #raw_input("请输入天数：")
-ret_list=[]
-#conn = sqlite3.connect("db")
-#cursor = conn.cursor()
-#cursor.execute('create table if not exists jj(id varchar(20) primary key, name varchar(20))')
-#cursor.execute('insert into jj(id,name) values ("321", "ms")')
-#cursor.execute('select * from jj where id=?', "321")
-#values = cursor.fetchall()
-#values
-#sys.exit("sorry, goodbye!")
+
+# 连接数据库，创建所需数据表（如果不存在的话）
+conn = sqlite3.connect('db')
+conn.row_factory = sqlite3.Row
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE if not exists stocks
+             (code char(6) PRIMARY KEY     NOT NULL,
+             name           TEXT    NOT NULL,
+             hold            INT     NOT NULL
+);''')
+cursor.execute('''CREATE TABLE if not exists stock_daily
+             (
+             code char(6) not null,
+             date int not null,
+             dwjz real not null,
+             ljjz real not null,
+             rzzl real not null,
+             unique (code, date)
+);''')
+conn.commit()
+
+# 获取stock列表
+fund_list = []
 file = open("list")
-fund_list = file.readlines()
+tmplist = file.readlines()
+for line in tmplist:
+    fund_list.append(line.strip())
+
+# 获取stock名称
+for i in fund_list:
+    cursor.execute('select count(*) from stocks where code=?', (i,))
+    row = cursor.fetchone()
+    if row[0] > 0:
+        print 'continue'
+        continue
+    name = getName(i)
+    cursor.execute('insert into stocks values(?,?,?)', (i, name, 0))
+    print "get name: " + i
+conn.commit()
+
+#获取stock每日数据
+for i in fund_list:
+    cursor.execute('select count(*) from stock_daily where code=?', (i,))
+    row = cursor.fetchone()
+    if row[0] > 0:
+        days = 6
+    else:
+        days = 1000
+    daily_data = getDailyData(i, days)
+    for d in daily_data:
+        cursor.execute('insert into stock_daily(code,date,dwjz,ljjz,rzzl) values(?,?,?,?,?)', 
+                   (i, d['date'], d['dwjz'], d['ljjz'], d['rzzl']))
+    print "get daily data: " + i
+conn.commit()
+
+conn.close()
+sys.exit('good bye')
+
+
+days = raw_input("请输入工作日天数：")
+ret_list=[]
 for fund_code in fund_list:
-    print fund_code.strip()
     fund_code = fund_code.strip()
     ret_list.append(getFundData(fund_code, days))
 
